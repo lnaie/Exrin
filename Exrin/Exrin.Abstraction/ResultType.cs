1456e6728aaec69b662acbe85a188b747cc39d85
Adam Pedley <adam.pedley@gmail.com>
2016-02-07 00:16:12 +1100

    More abstractions and upgrade ViewModelExecute

---
+++ Exrin/Exrin.Abstraction/ResultType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exrin.Abstraction
+{
+    /// <summary>
+    /// The result of the Execute function and what should be performed next
+    /// </summary>
+    public enum ResultType
+    {
+        None = 0,
+        Navigation = 1,
+        Display = 2
+    }
+}
--- Exrin/Exrin.Framework/ViewModelExecute.cs
+++ Exrin/Exrin.Framework/ViewModelExecute.cs
@@ -1,123 +1,152 @@
 ﻿using Exrin.Abstraction;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Exrin.Framework
 {
-    private static async Task ViewModelExecute(IExecutionComplete sender,
-             List<IOperation> operations,
-             Func<Task> notifyOfActivity = null,
-             Func<Task> notifyActivityFinished = null,
-             Func<Task<bool>> handleUnhandledException = null,
-             Func<Task> handleTimeout = null,
-             int timeoutMilliseconds = 0,
-             Func<Task> completed = null,
-             IApplicationInsights insights = null,
-             string name = ""
-             )
+
+
+    public static class Execution
     {
-        // If current executing, ignore the latest request
-        lock (sender)
+        public static Task ViewModelExecute(this IExecutionComplete sender, IViewModelExecute execute, [CallerMemberName] string name = "")
         {
-            if (!_status.ContainsKey(sender))
-                _status.Add(sender, true);
-            else if (_status[sender])
-                return;
-            else
-                _status[sender] = true;
+            return ViewModelExecute(sender,
+                                    operations: execute.Operations,
+                                    setResult: execute.SetResult,
+                                    handleTimeout: execute.HandleTimeout,
+                                    handleUnhandledException: execute.HandleUnhandledException,
+                                    insights: execute.Insights,
+                                    notifyActivityFinished: execute.NotifyActivityFinished,
+                                    notifyOfActivity: execute.NotifyOfActivity,
+                                    timeoutMilliseconds: execute.TimeoutMilliseconds,
+                                    name: name);
         }
 
-        try
+        private readonly static Dictionary<object, bool> _status = new Dictionary<object, bool>();
+
+        private static async Task ViewModelExecute(IExecutionComplete sender,
+                 List<IOperation> operations,
+                 Func<Task> notifyOfActivity = null,
+                 Func<Task> notifyActivityFinished = null,
+                 Func<Task<bool>> handleUnhandledException = null,
+                 Func<Task> handleTimeout = null,
+                 int timeoutMilliseconds = 0,
+                 Func<Task> setResult = null,
+                 IApplicationInsights insights = null,
+                 string name = ""
+                 )
         {
-            if (insights != null)
-                insights.TrackEvent(name, $"User activated {name}");
-        }
-        catch (Exception ex)
-        {
-            Debug.WriteLine($"insights.TrackEvent({name}) {ex.Message}");
-        }
-
-        List<Func<Task>> rollbacks = new List<Func<Task>>();
-        bool transactionRunning = false;
-
-        // Setup Cancellation of Tasks if long running
-        var task = new CancellationTokenSource();
-
-        if (timeoutMilliseconds > 0)
-        {
-            if (handleTimeout != null)
-                task.Token.Register(async () => { await handleTimeout(); });
-            else if (handleUnhandledException != null)
-                task.Token.Register(async () => { await handleUnhandledException(); });
-            else
-                throw new Exception($"You must specify either {nameof(handleTimeout)} or {nameof(handleUnhandledException)} to handle a timeout.");
-
-            task.CancelAfter(timeoutMilliseconds);
-        }
-
-        try
-        {
-            if (notifyOfActivity == null)
-                throw new Exception($"{nameof(notifyOfActivity)} is null: You must notify the user that something is happening");
-
-            await notifyOfActivity();
-
-            // Transaction Block
-            transactionRunning = true;
-
-            foreach (var operation in operations)
+            // If current executing, ignore the latest request
+            lock (sender)
             {
-                rollbacks.Add(operation.Rollback);
-
-                if (operation.Function != null)
-                    await await RunOnBackgroundThreadAsync(operation.Function, task.Token); // But wait for it to finish
-
-                if (!operation.ChainedRollback)
-                    rollbacks.Remove(operation.Rollback);
+                if (!_status.ContainsKey(sender))
+                    _status.Add(sender, true);
+                else if (_status[sender])
+                    return;
+                else
+                    _status[sender] = true;
             }
 
-            rollbacks.Clear();
-            transactionRunning = false;
-            // End of Transaction Block
+            // Background thread
+            Task insight = Task.Run(() =>
+            {
+                try
+                {
+                    if (insights != null)
+                        insights.TrackEvent(name, $"User activated {name}");
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine($"insights.TrackEvent({name}) {ex.Message}");
+                }
+            });
 
-            if (notifyActivityFinished == null)
-                throw new Exception($"{nameof(notifyActivityFinished)} is null: You need to specify what happens when the operations finish");
+            List<Func<Task>> rollbacks = new List<Func<Task>>();
+            bool transactionRunning = false;
 
-            await notifyActivityFinished();
+            // Setup Cancellation of Tasks if long running
+            var task = new CancellationTokenSource();
 
-            if (completed != null)
-                await completed();
-        }
-        catch
-        {
-            if (handleUnhandledException == null)
-                throw;
+            if (timeoutMilliseconds > 0)
+            {
+                if (handleTimeout != null)
+                    task.Token.Register(async () => { await handleTimeout(); });
+                else if (handleUnhandledException != null)
+                    task.Token.Register(async () => { await handleUnhandledException(); });
+                else
+                    throw new Exception($"You must specify either {nameof(handleTimeout)} or {nameof(handleUnhandledException)} to handle a timeout.");
 
-            var handled = await handleUnhandledException();
-            if (!handled)
-                throw;
-        }
-        finally
-        {
+                task.CancelAfter(timeoutMilliseconds);
+            }
+
             try
             {
-                if (transactionRunning)
+                if (notifyOfActivity == null)
+                    throw new Exception($"{nameof(notifyOfActivity)} is null: You must notify the user that something is happening");
+
+                await notifyOfActivity();
+
+                // Transaction Block
+                transactionRunning = true;
+
+                foreach (var operation in operations)
                 {
-                    rollbacks.Reverse(); // Do rollbacks in reverse order
-                    foreach (var rollback in rollbacks)
-                        await rollback();
+                    rollbacks.Add(operation.Rollback);
+
+                    if (operation.Function != null)
+                        await Task.Run(operation.Function, task.Token); // Background Thread
+
+                    if (!operation.ChainedRollback)
+                        rollbacks.Remove(operation.Rollback);
                 }
 
-                await sender.HandleResult();
+                rollbacks.Clear();
+                transactionRunning = false;
+                // End of Transaction Block
+
+                if (notifyActivityFinished == null)
+                    throw new Exception($"{nameof(notifyActivityFinished)} is null: You need to specify what happens when the operations finish");
+
+                await notifyActivityFinished();
+
+                if (setResult != null)
+                    await setResult();
+            }
+            catch
+            {
+                if (handleUnhandledException == null)
+                    throw;
+
+                var handled = await handleUnhandledException();
+                if (!handled)
+                    throw;
             }
             finally
             {
-                _status.Remove(sender);
-            }
+                try
+                {
+                    if (transactionRunning)
+                    {
+                        rollbacks.Reverse(); // Do rollbacks in reverse order
+                        foreach (var rollback in rollbacks)
+                            await rollback();
+                    }
 
+                    await sender.HandleResult();
+                }
+                finally
+                {
+                    _status.Remove(sender);
+                }
+
+            }
         }
+
     }
 }
