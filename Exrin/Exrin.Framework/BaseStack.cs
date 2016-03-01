01006ad930ad96dcbfb0cb3114285cc8f3a79486
Adam Pedley <adam.pedley@gmail.com>
2016-03-02 09:26:03 +1100

    Stacks

--- Exrin/Exrin.Framework/BaseStack.cs
+++ Exrin/Exrin.Framework/BaseStack.cs
@@ -8,41 +8,35 @@ using System.Threading.Tasks;
 namespace Exrin.Framework
 {
         
-    public class BaseStack
+    public class BaseStack: IStack
     {
-        protected INavigationPage _container = null;
+        //protected INavigationPage _container = null;
         protected INavigationService _navigationService = null;
         private IDisplayService _displayService = null;
         protected IPageService _pageService = null;
-
-        private bool _isFirstRun = false;
 
         public BaseStack(INavigationService navigationService, IDisplayService dialogService, IPageService pageService)
         {
             _navigationService = navigationService;
             _displayService = dialogService;
             _pageService = pageService;
+
+            MapPages();
+            MapViewModels();
+        }
+
+        public async Task StartNavigation(object args = null)
+        {
+            await _navigationService.Navigate(NavigationStartPageKey, args);
         }
 
         /// <summary>
         /// Will register appropriate Services for Dependency Injection.
         /// </summary>
-        public async Task Init(object navigationArgs = null)
+        protected void SetContainer(INavigationPage container)
         {
-
-            _navigationService.Init(_container);
-            _displayService.Init(_container);
-
-            if (!_isFirstRun)
-            {
-                MapPages();
-                MapViewModels();
-
-                await _navigationService.Navigate(NavigationStartPageKey, navigationArgs);
-
-                _isFirstRun = true;
-            }
-
+            _navigationService.Init(container);
+            _displayService.Init(container);
         }
 
         protected virtual void MapPages() { }
