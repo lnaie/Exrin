7f8ce857b049c641a6f1f2e07571f43d870c5b5b
Adam Pedley <adam.pedley@gmail.com>
2016-03-01 18:39:57 +1100

    DisplayService

---
+++ Exrin/Exrin.Framework/DisplayService.cs
@@ -0,0 +1,23 @@
+﻿using Exrin.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exrin.Framework
+{
+    public class DisplayService : IDisplayService
+    {
+
+        public void Init(INavigationPage page)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ShowDialog(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
