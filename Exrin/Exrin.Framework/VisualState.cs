312f69a3605c4c0bdc11ad727dc16ada2322a965
Adam Pedley <adam.pedley@gmail.com>
2017-02-27 17:09:59 +1100

    Initial Property Map

--- Exrin/Exrin.Framework/VisualState.cs
+++ Exrin/Exrin.Framework/VisualState.cs
@@ -12,14 +12,34 @@
 
         public VisualState(IBaseModel model)
         {
-			if (model == null)
-				throw new ArgumentNullException($"{nameof(VisualState)} can not have a null {nameof(IBaseModel)}");
+            if (model == null)
+                throw new ArgumentNullException($"{nameof(VisualState)} can not have a null {nameof(IBaseModel)}");
 
             Model = model;
             HookEvents();
+            MapProperties();
         }
 
-		public virtual void Init() { }
+        /// <summary>
+        /// Will copy property values, on first load as PropertyChanged event won't be initially called.
+        /// </summary>
+        private void MapProperties()
+        {
+            if (Model?.ModelState != null)
+                foreach (var property in Model.ModelState.GetType().GetRuntimeProperties())
+                {
+                    try
+                    {
+                        this.GetType().GetRuntimeProperty(property.Name)?.SetValue(this, property.GetValue(Model.ModelState));
+                    }
+                    catch
+                    {
+                        Debug.WriteLine($"Binding of the {nameof(ModelState)} to {nameof(VisualState)} for property {property.Name} failed.");
+                    }
+                }
+        }
+
+        public virtual void Init() { }
 
         private bool _isBusy = false;
         public bool IsBusy { get { return _isBusy; } set { _isBusy = value; OnPropertyChanged(); } }
@@ -29,26 +49,26 @@
         /// </summary>
         /// <param name="propertyName"></param>
 		protected virtual void OnModelStatePropertyChanged(string propertyName)
-		{
-			try
-			{
-				var modelState = Model.GetType().GetRuntimeProperty("ModelState").GetValue(Model);
+        {
+            try
+            {
+                var modelState = Model.GetType().GetRuntimeProperty("ModelState").GetValue(Model);
 
-				var value = modelState.GetType().GetRuntimeProperty(propertyName).GetValue(modelState);
+                var value = modelState.GetType().GetRuntimeProperty(propertyName).GetValue(modelState);
 
                 var existingValue = this.GetType().GetRuntimeProperty(propertyName)?.GetValue(this);
 
                 if (existingValue != value)
                     this.GetType().GetRuntimeProperty(propertyName)?.SetValue(this, value);
-			}
-			catch
-			{
-				Debug.WriteLine($"Binding of the {nameof(ModelState)} to {nameof(VisualState)} for property {propertyName} failed.");
-			}
-			
-		}
+            }
+            catch
+            {
+                Debug.WriteLine($"Binding of the {nameof(ModelState)} to {nameof(VisualState)} for property {propertyName} failed.");
+            }
 
-		private void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
+        }
+
+        private void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
         {
             OnModelStatePropertyChanged(e.PropertyName);
         }
