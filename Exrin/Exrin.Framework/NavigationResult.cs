e91f17a356f3ee5e1ee12cc41ffc2ed6a0a156ec
Adam Pedley <adam.pedley@gmail.com>
2017-03-06 11:34:49 +1100

    NavigationResult (syntatic sugar)

---
+++ Exrin/Exrin.Framework/NavigationResult.cs
@@ -0,0 +1,18 @@
+﻿namespace Exrin.Framework
+{
+    public class NavigationResult: Result
+    {
+        public NavigationResult(object stackType, object key )
+        {
+            base.ResultAction = Abstraction.ResultType.Navigation;
+            base.Arguments = new NavigationArgs() { StackType = stackType, Key = key };
+        }
+
+        public NavigationResult(object stackType, object key, object parameter, bool newInstance)
+        {
+            base.ResultAction = Abstraction.ResultType.Navigation;
+            base.Arguments = new NavigationArgs() { StackType = stackType, Key = key, Parameter = parameter, NewInstance = newInstance };
+        }
+
+    }
+}
--- Exrin/Exrin.Framework/Result.cs
+++ Exrin/Exrin.Framework/Result.cs
@@ -7,5 +7,13 @@
         public IResultArgs Arguments { get; set; }
 
         public ResultType ResultAction { get; set; }
+
+        public Result() { }
+
+        public Result(ResultType resultAction, IResultArgs arguments)
+        {
+            ResultAction = resultAction;
+            Arguments = arguments;
+        }
     }
 }
