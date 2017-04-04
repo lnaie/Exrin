57ecdcdcfc77532dd03c836949171e0300f6e5f1
Adam Pedley <adam.pedley@gmail.com>
2017-04-04 13:42:54 +1000

    GetAssemblyName Extension

--- Exrin/Exrin.Framework/Extensions.cs
+++ Exrin/Exrin.Framework/Extensions.cs
@@ -2,12 +2,18 @@
 {
     using Abstraction;
     using System.Collections.Generic;
-    
+    using System.Reflection;
+
     public static class Extensions
     {
         public static IList<IResult> ToList(this Result result)
         {
             return new List<IResult>() { result };
+        }
+
+        public static AssemblyName GetAssemblyName(object instance)
+        {
+            return instance.GetType().GetTypeInfo().Assembly.GetName();
         }
     }
 }
