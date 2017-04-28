6e18a902a29f42e7abcee2a83aa09c56cb3da8a8
Adam Pedley <adam.pedley@gmail.com>
2017-04-28 16:18:14 +1000

    Binding and OnPropertyChanged Equality Checks No longer raises OnPropertyChanged if no change

--- Exrin/Exrin.Framework/BindableModel.cs
+++ Exrin/Exrin.Framework/BindableModel.cs
@@ -1,90 +1,101 @@
 ﻿namespace Exrin.Framework
 {
-    using System;
-    using System.Collections.Generic;
-    using System.ComponentModel;
-    using System.Runtime.CompilerServices;
+	using System;
+	using System.Collections.Generic;
+	using System.ComponentModel;
+	using System.Runtime.CompilerServices;
 
-    public class BindableModel : INotifyPropertyChanged, IDisposable
-    {
-        private IDictionary<DateTime, KeyValuePair<string, object>> _propertyValueTrack = null;
-        private IDictionary<string, object> _propertyValues = null;
+	public class BindableModel : INotifyPropertyChanged, IDisposable
+	{
+		private IDictionary<DateTime, KeyValuePair<string, object>> _propertyValueTrack = null;
+		private IDictionary<string, object> _propertyValues = null;
 
-        public BindableModel()
-        {
-            _propertyValues = new Dictionary<string, object>();
-            _propertyValueTrack = new Dictionary<DateTime, KeyValuePair<string, object>>();
-        }
+		public BindableModel()
+		{
+			_propertyValues = new Dictionary<string, object>();
+			_propertyValueTrack = new Dictionary<DateTime, KeyValuePair<string, object>>();
+		}
 
-        //TODO: When C#7 is released, possible replace with Sideways Loading for INPC
+		//TODO: When C#7 is released, possible replace with Sideways Loading for INPC
 
-        public T Get<T>([CallerMemberName] string propertyName = "")
-        {
-            if (_propertyValues.ContainsKey(propertyName))
-                return (T)_propertyValues[propertyName];
-            else
-                return default(T);
-        }
+		public T Get<T>([CallerMemberName] string propertyName = "")
+		{
+			if (_propertyValues.ContainsKey(propertyName))
+				return (T)_propertyValues[propertyName];
+			else
+				return default(T);
+		}
 
-        public void Set(object value, [CallerMemberName] string propertyName = "")
-        {
+		public void Set(object value, [CallerMemberName] string propertyName = "")
+		{
 
-            object oldValue = null; // Only works with value types
-            if (_propertyValues.ContainsKey(propertyName))
-            {
-                oldValue = _propertyValues[propertyName];
-                if (oldValue == value)
-                    return;
+			object oldValue = null; // Only works with value types
+			if (_propertyValues.ContainsKey(propertyName))
+			{
+				oldValue = _propertyValues[propertyName];
+				if (oldValue == value)
+					return;
 
-                _propertyValues[propertyName] = value;
-            }
-            else
-                _propertyValues.Add(propertyName, value);
+				_propertyValues[propertyName] = value;
+			}
+			else
+				_propertyValues.Add(propertyName, value);
 
-            if (App.PlatformOptions.StateTracking)
-                _propertyValueTrack.Add(DateTime.Now, new KeyValuePair<string, object>(propertyName, value));
+			if (App.PlatformOptions.StateTracking)
+				_propertyValueTrack.Add(DateTime.Now, new KeyValuePair<string, object>(propertyName, value));
 
-            OnPropertyChanged(oldValue, value, propertyName);
-        }
+			if (oldValue == null && value == null)
+				return;
 
-        public IDictionary<DateTime, KeyValuePair<string, object>> GetStateHistory()
-        {
-            return _propertyValueTrack;
-        }
+			if ((oldValue == null && value != null)
+				|| (oldValue != null && value == null))
+			{
+				OnPropertyChanged(oldValue, value, propertyName);
+				return;
+			}
 
-        public event PropertyChangedEventHandler PropertyChanged;
+			if (!oldValue.Equals(value)) // Only when property changed
+				OnPropertyChanged(oldValue, value, propertyName);
+		}
 
-        public void OnPropertyChanged([CallerMemberName] string name = "")
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-        }
+		public IDictionary<DateTime, KeyValuePair<string, object>> GetStateHistory()
+		{
+			return _propertyValueTrack;
+		}
 
-        public void OnPropertyChanged(object oldValue, object newValue, [CallerMemberName] string name = "")
-        {
-            PropertyChanged?.Invoke(this, new PropertyValueChangedEventArgs(name, oldValue, newValue));
-        }
+		public event PropertyChangedEventHandler PropertyChanged;
 
-        public virtual void Disposing()
-        {
+		public void OnPropertyChanged([CallerMemberName] string name = "")
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+		}
 
-        }
+		public void OnPropertyChanged(object oldValue, object newValue, [CallerMemberName] string name = "")
+		{
+			PropertyChanged?.Invoke(this, new PropertyValueChangedEventArgs(name, oldValue, newValue));
+		}
 
-        private bool _disposed = false;
-        protected void Dispose(bool disposing)
-        {
-            if (!_disposed)
-            {
-                if (disposing)
-                {
-                    Disposing();
-                }
-                _disposed = true;
-            }
-        }
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
-    }
+		public virtual void Disposing()
+		{
+
+		}
+
+		private bool _disposed = false;
+		protected void Dispose(bool disposing)
+		{
+			if (!_disposed)
+			{
+				if (disposing)
+				{
+					Disposing();
+				}
+				_disposed = true;
+			}
+		}
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+	}
 }
--- Exrin/Exrin.Framework/VisualState.cs
+++ Exrin/Exrin.Framework/VisualState.cs
@@ -1,131 +1,160 @@
 ﻿namespace Exrin.Framework
 {
-    using Abstraction;
-    using System;
-    using System.ComponentModel;
-    using System.Diagnostics;
-    using System.Reflection;
+	using Abstraction;
+	using System;
+	using System.ComponentModel;
+	using System.Diagnostics;
+	using System.Reflection;
 
-    public class VisualState : BindableModel, IVisualState
-    {
-        protected IBaseModel Model { get; set; }
+	public class VisualState : BindableModel, IVisualState
+	{
+		protected IBaseModel Model { get; set; }
 
-        public VisualState(IBaseModel model)
-        {
-            Model = model ?? throw new ArgumentNullException($"{nameof(VisualState)} can not have a null {nameof(IBaseModel)}");
+		public VisualState(IBaseModel model)
+		{
+			Model = model ?? throw new ArgumentNullException($"{nameof(VisualState)} can not have a null {nameof(IBaseModel)}");
 
-            HookEvents();
-            MapProperties();
-        }
+			HookEvents();
+			MapProperties();
+		}
 
-        /// <summary>
-        /// Will copy property values, on first load as PropertyChanged event won't be initially called.
-        /// </summary>
-        private void MapProperties()
-        {
-            if (Model?.ModelState != null)
-                foreach (var property in Model.ModelState.GetType().GetRuntimeProperties())
-                {
-                    try
-                    {
-                        this.GetType().GetRuntimeProperty(property.Name)?.SetValue(this, property.GetValue(Model.ModelState));
-                    }
-                    catch
-                    {
-                        Debug.WriteLine($"Binding of the {nameof(ModelState)} to {nameof(VisualState)} for property {property.Name} failed.");
-                    }
-                }
-        }
+		/// <summary>
+		/// Will copy property values, on first load as PropertyChanged event won't be initially called.
+		/// </summary>
+		private void MapProperties()
+		{
+			if (Model?.ModelState != null)
+				foreach (var property in Model.ModelState.GetType().GetRuntimeProperties())
+				{
+					try
+					{
+						this.GetType().GetRuntimeProperty(property.Name)?.SetValue(this, property.GetValue(Model.ModelState));
+					}
+					catch
+					{
+						Debug.WriteLine($"Binding of the {nameof(ModelState)} to {nameof(VisualState)} for property {property.Name} failed.");
+					}
+				}
+		}
 
-        public virtual void Init() { }
+		public virtual void Init() { }
 
-        private bool _isBusy = false;
-        public bool IsBusy { get { return _isBusy; } set { _isBusy = value; OnPropertyChanged(); } }
+		private bool _isBusy = false;
+		public bool IsBusy { get { return _isBusy; } set { _isBusy = value; OnPropertyChanged(); } }
 
-        /// <summary>
-        /// This monitors properties in Visual State marked as 2-way binding and propagates to the Model
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void VisualState_PropertyChanged(object sender, PropertyChangedEventArgs e)
-        {
-            try
-            {
-                // Get ModelState
-                var modelState = Model.GetType().GetRuntimeProperty("ModelState").GetValue(Model);
+		/// <summary>
+		/// This monitors properties in Visual State marked as 2-way binding and propagates to the Model
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void VisualState_PropertyChanged(object sender, PropertyChangedEventArgs e)
+		{
+			try
+			{
+				// Get ModelState
+				var modelState = Model.GetType().GetRuntimeProperty("ModelState").GetValue(Model);
 
-                // Get VisualState Property
-                var visualStateProperty = this.GetType().GetRuntimeProperty(e.PropertyName);
-                if (visualStateProperty == null)
-                    return;
+				// Get VisualState Property
+				var visualStateProperty = this.GetType().GetRuntimeProperty(e.PropertyName);
+				if (visualStateProperty == null)
+					return;
 
-                var attribute = visualStateProperty.GetCustomAttribute(typeof(BindingAttribute)) as BindingAttribute;
-                if (attribute == null)
-                    return; // No 2 way binding
+				var attribute = visualStateProperty.GetCustomAttribute(typeof(BindingAttribute)) as BindingAttribute;
+				if (attribute == null)
+					return; // No 2 way binding
 
-                if (attribute.BindingType != BindingType.TwoWay)
-                    return; // No 2 way binding
+				if (attribute.BindingType != BindingType.TwoWay)
+					return; // No 2 way binding
 
-                // Set property on ModelState
-                var value = visualStateProperty.GetValue(this);
-                modelState.GetType().GetRuntimeProperty(e.PropertyName)?.SetValue(modelState, value);
-            }
-            catch
-            {
-                Debug.WriteLine($"Binding of the {nameof(VisualState)} to {nameof(ModelState)} for property {e.PropertyName} failed.");
-            }
-        }
+				// Set property on ModelState, only when different
+				var value = visualStateProperty.GetValue(this);
+				var property = modelState.GetType().GetRuntimeProperty(e.PropertyName);
+				if (property != null)
+				{
+					var modelStateValue = property.GetValue(modelState);
+
+					if (modelStateValue == null && value == null)
+						return;
+
+					if ((modelStateValue == null && value != null)
+						|| (modelStateValue != null && value == null))
+					{
+						property.SetValue(modelState, value);
+						return;
+					}
+
+					if (!modelStateValue.Equals(value))
+						property.SetValue(modelState, value);
+				}
+			}
+			catch
+			{
+				Debug.WriteLine($"Binding of the {nameof(VisualState)} to {nameof(ModelState)} for property {e.PropertyName} failed.");
+			}
+		}
 
 
-        /// <summary>
-        /// Will copy the value from the Model State into the Visual State when the Model property changes
-        /// </summary>
-        /// <param name="propertyName"></param>
+		/// <summary>
+		/// Will copy the value from the Model State into the Visual State when the Model property changes
+		/// </summary>
+		/// <param name="propertyName"></param>
 		protected virtual void OnModelStatePropertyChanged(string propertyName)
-        {
-            try
-            {
-                var modelState = Model.GetType().GetRuntimeProperty("ModelState").GetValue(Model);
+		{
+			try
+			{
+				var modelState = Model.GetType().GetRuntimeProperty("ModelState").GetValue(Model);
 
-                var value = modelState.GetType().GetRuntimeProperty(propertyName).GetValue(modelState);
+				var value = modelState.GetType().GetRuntimeProperty(propertyName).GetValue(modelState);
 
-                var existingValue = this.GetType().GetRuntimeProperty(propertyName)?.GetValue(this);
+				var property = this.GetType().GetRuntimeProperty(propertyName);
+				if (property == null)
+					return;
 
-                if (existingValue != value)
-                    this.GetType().GetRuntimeProperty(propertyName)?.SetValue(this, value);
-            }
-            catch
-            {
-                Debug.WriteLine($"Binding of the {nameof(ModelState)} to {nameof(VisualState)} for property {propertyName} failed.");
-            }
+				var existingValue = property?.GetValue(this);
 
-        }
+				if (existingValue == null && value == null)
+					return;
 
-        private void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
-        {
-            OnModelStatePropertyChanged(e.PropertyName);
-        }
+				if ((existingValue == null && value != null)
+					|| existingValue != null && value == null)
+				{
+					property.SetValue(this, value);
+					return;
+				}
 
-        protected void HookEvents()
-        {
-            this.PropertyChanged += VisualState_PropertyChanged;
-            if (Model?.ModelState != null)
-                Model.ModelState.PropertyChanged += OnPropertyChanged;
-        }
+				if (!existingValue.Equals(value))
+					property.SetValue(this, value);
+			}
+			catch
+			{
+				Debug.WriteLine($"Binding of the {nameof(ModelState)} to {nameof(VisualState)} for property {propertyName} failed.");
+			}
 
+		}
 
+		private void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
+		{
+			OnModelStatePropertyChanged(e.PropertyName);
+		}
 
-        public override void Disposing()
-        {
-            this.PropertyChanged -= VisualState_PropertyChanged;
-            if (Model?.ModelState != null)
-                Model.ModelState.PropertyChanged -= OnPropertyChanged;
-        }
+		protected void HookEvents()
+		{
+			this.PropertyChanged += VisualState_PropertyChanged;
+			if (Model?.ModelState != null)
+				Model.ModelState.PropertyChanged += OnPropertyChanged;
+		}
 
-        ~VisualState()
-        {
-            Dispose(false);
-        }
+		public override void Disposing()
+		{
+			this.PropertyChanged -= VisualState_PropertyChanged;
+			if (Model?.ModelState != null)
+				Model.ModelState.PropertyChanged -= OnPropertyChanged;
+		}
 
-    }
+		~VisualState()
+		{
+			Dispose(false);
+		}
+
+	}
 }
