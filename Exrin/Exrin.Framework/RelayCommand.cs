a265e0e04b64204445bc7e44ca61288a3008af72
Adam Pedley <adam.pedley@gmail.com>
2017-07-13 21:04:00 +1000

    Added generics to RelayCommand

--- Exrin/Exrin.Framework/RelayCommand.cs
+++ Exrin/Exrin.Framework/RelayCommand.cs
@@ -7,63 +7,74 @@
 	using System.Collections.Generic;
 	using System.Threading;
 
+
+	public class RelayCommand<T> : RelayCommand
+	{
+		public RelayCommand(Func<T, Task> action) : base(new Action<object>(o => action((T)o))) { }
+		public RelayCommand(Func<T, Task> action, Func<object, bool> canExecute) : base(new Action<object>(o => action((T)o)), canExecute) { }
+		public RelayCommand(Action<T> action) : base(new Action<object>(o => action((T)o))) { }
+		public RelayCommand(Action<T> action, Func<object, bool> canExecute) : base(new Action<object>(o => action((T)o)), canExecute) { }
+
+	}
+
+
 	public class RelayCommand : ICommand, IRelayCommand
-    {
-        private readonly Func<object, bool> _canExecute = null;
-        private readonly Func<object, Task> _action = null;
-        public RelayCommand(Func<object, Task> action)
-        {
-            _action = action;
-        }
-        public RelayCommand(Func<object, Task> action, Func<object, bool> canExecute)
-        {
-            _action = action;
-            _canExecute = canExecute;
-        }        
-        public RelayCommand(Action<object> action)
-        {
-            _action = (parameter) => { action(parameter);  return Task.FromResult(true); };
-        }
-        public RelayCommand(Action<object> action, Func<object, bool> canExecute)
-        {
-            _action = (parameter) => { action(parameter); return Task.FromResult(true); };
-            _canExecute = canExecute;
-        }
+	{
+		private readonly Func<object, bool> _canExecute = null;
+		private readonly Func<object, Task> _action = null;
+		public RelayCommand(Func<object, Task> action)
+		{
+			_action = action;
+		}
+		public RelayCommand(Func<object, Task> action, Func<object, bool> canExecute)
+		{
+			_action = action;
+			_canExecute = canExecute;
+		}
+		public RelayCommand(Action<object> action)
+		{
+			_action = (parameter) => { action(parameter); return Task.FromResult(true); };
+		}
+		public RelayCommand(Action<object> action, Func<object, bool> canExecute)
+		{
+			_action = (parameter) => { action(parameter); return Task.FromResult(true); };
+			_canExecute = canExecute;
+		}
 
-        public void OnCanExecuteChanged()
-        {
-            CanExecuteChanged?.Invoke(this, new EventArgs());
-        } 
+		public void OnCanExecuteChanged()
+		{
+			CanExecuteChanged?.Invoke(this, new EventArgs());
+		}
 
-        public bool Executing { get; private set; } = false;
-        public Action FinishedCallback { get; set; } = null;
-        public int Timeout { get; set; }
+		public bool Executing { get; private set; } = false;
+		public Action FinishedCallback { get; set; } = null;
+		public int Timeout { get; set; }
 
 		public Func<object, CancellationToken, Task<IList<IResult>>> Function { get; internal set; }
 
 		public event EventHandler CanExecuteChanged;
 
-        public bool CanExecute(object parameter)
-        {
-            if (_canExecute == null)
-                return true;
+		public bool CanExecute(object parameter)
+		{
+			if (_canExecute == null)
+				return true;
 
-            return _canExecute.Invoke(parameter);
-        }
+			return _canExecute.Invoke(parameter);
+		}
 
-        public void Execute(object parameter)
-        {
+		public void Execute(object parameter)
+		{
 
-            if (!CanExecute(parameter))
-                return; 
+			if (!CanExecute(parameter))
+				return;
 
-            Executing = true;
+			Executing = true;
 
-            _action(parameter).ContinueWith((task) =>
-            {
-                Executing = false;
-                FinishedCallback?.Invoke();               
-            });        
-        }
-    }
+			_action(parameter).ContinueWith((task) =>
+			{
+				Executing = false;
+				FinishedCallback?.Invoke();
+			});
+		}
+	}
 }
